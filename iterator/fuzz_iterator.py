113c65380b33a4ec64975f8eeb48823e47cbb33f
ch097711 <ch097711@gmail.com>
2025-03-02 15:00:52 +0800

    feat: Add MIN_COVERAGE_IMPROVEMENT configuration and update FuzzIterator logic

--- iterator/fuzz_iterator.py
+++ iterator/fuzz_iterator.py
@@ -1,5 +1,6 @@
 import logging
 
+from config.config import MIN_COVERAGE_IMPROVEMENT
 from external.introspector import Introspector
 
 logger = logging.getLogger(__name__)
@@ -10,7 +11,6 @@ class FuzzIterator:
         self.project = project
         self.introspector = Introspector()
         self.coverages: list[float] = []
-        self.min_improvement = 0.01  # Minimum coverage improvement to consider progress
 
     def record_cov(self):
         get_cov = self.introspector.line_coverage(self.project)
@@ -33,7 +33,7 @@ class FuzzIterator:
 
         current_cov = self.coverages[-1]
         previous_cov = self.coverages[-2]
-        if current_cov - previous_cov > self.min_improvement:
+        if current_cov - previous_cov > MIN_COVERAGE_IMPROVEMENT:
             return False  # Coverage is still improving, continue mutation
 
         return True  # Coverage has stagnated, should regenerate
