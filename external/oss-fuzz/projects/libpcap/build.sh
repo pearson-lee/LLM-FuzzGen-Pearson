8ffbfb802f97a8b08a4491a5cb4773326af433f0
ch097711 <ch097711@gmail.com>
2025-06-25 16:30:02 +0800

    init libpcap

---
+++ external/oss-fuzz/projects/libpcap/build.sh
@@ -0,0 +1,80 @@
+#!/bin/bash -eu
+# Copyright 2018 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+##### LLM-FuzzGen #####
+export PATH="/ccache/bin:$PATH" # Use ccache for faster builds, from oss-fuzz/infra/base-images/base-builder/Dockerfile
+export CCACHE_DIR="$WORK/ccache"
+export CFLAGS="$CFLAGS -w -fno-color-diagnostics -fdiagnostics-fixit-info" # Suppress warnings, color diagnostics and fixit info
+export CXXFLAGS="$CXXFLAGS -w -fno-color-diagnostics -fdiagnostics-fixit-info" # Suppress warnings, color diagnostics and fixit info
+#https://github.com/google/oss-fuzz/pull/10891
+#if [ "$SANITIZER" == "introspector" ]; then
+#  export CFLAGS="${CFLAGS} -fsanitize=address"
+#  export CXXFLAGS="${CXXFLAGS} -fsanitize=address"
+#fi
+#https://github.com/google/oss-fuzz/pull/12356
+if [ "$SANITIZER" == "introspector" ]; then
+  export CFLAGS=$(echo "$CFLAGS" | sed 's/gold/lld/g')
+  export CXXFLAGS=$(echo "$CXXFLAGS" | sed 's/gold/lld/g')
+fi
+#######################
+cd libpcap
+# build project
+mkdir build
+cd build
+cmake ..
+make
+
+
+# build fuzz targets
+for target in pcap filter both
+do
+    $CC $CFLAGS -I.. -c ../testprogs/fuzz/fuzz_$target.c -o fuzz_$target.o
+    $CXX $CXXFLAGS fuzz_$target.o -o $OUT/fuzz_$target -Wl,--whole-archive libpcap.a -Wl,--no-whole-archive $LIB_FUZZING_ENGINE
+done
+
+##### LLM-FuzzGen #####
+# Compile llm_fuzzgen*.cc, llm_fuzzgen*.cpp, llm_fuzzgen*.c
+find "$SRC" -maxdepth 1 -type f \( -name "llm_fuzzgen*.c" -o -name "llm_fuzzgen*.cc" -o -name "llm_fuzzgen*.cpp" \) -print | while read -r target; do
+  target_basename=$(basename "${target%.*}")
+
+  #### compile the fuzz target
+  $CC $CFLAGS -I.. -c "$target" -o $target_basename.o
+  $CXX $CXXFLAGS $target_basename.o -o $OUT/$target_basename -Wl,--whole-archive libpcap.a -Wl,--no-whole-archive $LIB_FUZZING_ENGINE
+  ####
+
+  if [ -f "$SRC/llm_fuzzgen.dict" ] && [ ! -f "$SRC/${target_basename}.options" ]; then
+    echo "[libfuzzer]" > "$SRC/${target_basename}.options"
+    echo "dict = llm_fuzzgen.dict" >> "$SRC/${target_basename}.options"
+  fi
+done
+cp $SRC/llm_fuzzgen.dict $OUT/ || true
+cp $SRC/llm_fuzzgen*.options $OUT/ || true
+cp $SRC/llm_fuzzgen*_seed_corpus.zip $OUT/ || true
+#######################
+
+# export other associated stuff
+cd ..
+cp testprogs/fuzz/fuzz_*.options $OUT/
+# builds corpus
+cd $SRC/tcpdump/
+zip -r fuzz_pcap_seed_corpus.zip tests/
+cp fuzz_pcap_seed_corpus.zip $OUT/
+cd $SRC/libpcap/testprogs/BPF
+mkdir corpus
+ls *.txt | while read i; do tail -1 $i > corpus/$i; done
+zip -r fuzz_filter_seed_corpus.zip corpus/
+cp fuzz_filter_seed_corpus.zip $OUT/
+
